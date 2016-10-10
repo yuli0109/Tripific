e2ccff87907c5fcf8a843327dffceb1098a159e3
Victoria Dunham <vkurzweg@gmail.com>
2016-10-10 12:30:22 -0700

    merge

---
+++ seeds.js
@@ -0,0 +1,7 @@
+// run this file with npm run seed
+var db = require('./config/database');
+var User = require('./models/user');
+
+// User.remove({}).then(process.exit);
+
+// var u1 = User.findOne({}).then(console.log).then(process.exit);
---
+++ test.js
@@ -0,0 +1,43 @@
+// run this file with npm run test
+
+var db = require('./config/database');
+var User = require('./models/user');
+
+
+// clean out test db
+Promise.all([
+  User.remove()
+]).then(function(){
+  // we can instatiate and save a user to the db.
+  var user = new User({
+    name: 'Victoria'
+  })
+  return user.save()
+}).then(function(){
+  // find user in db
+  return User.findOne({})
+}).then(function(user){
+  // username should be db if it saved.
+  console.log(user.name, 'Victoria');
+})
+.catch(console.log)
+.then(process.exit)
+
+// clean out test db
+Promise.all([
+  Trip.remove()
+]).then(function(){
+  // we can instatiate and save a user to the db.
+  var user = new User({
+    date: 10/10/2016
+  })
+  return user.save()
+}).then(function(){
+  // find user in db
+  return User.findOne({})
+}).then(function(user){
+  // username should be db if it saved.
+  console.log(trip.date, 10/10/2016);
+})
+.catch(console.log)
+.then(process.exit)
