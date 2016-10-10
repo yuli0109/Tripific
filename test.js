ab7bbd4ce16df1353d0c7a08fc1c4412e675a1c7
Victoria Dunham <vkurzweg@gmail.com>
2016-10-10 14:14:08 -0700

    add test

--- test.js
+++ test.js
@@ -2,42 +2,55 @@
 
 var db = require('./config/database');
 var User = require('./models/user');
+var Trip = require('./models/trip');
+var Stop = require('./models/stop');
 
 
 // clean out test db
 Promise.all([
-  User.remove()
-]).then(function(){
-  // we can instatiate and save a user to the db.
+  User.remove(),
+  Trip.remove(),
+  Stop.remove()
+])
+.then(function(){
+  // we can instatiate and save to the db.
   var user = new User({
     name: 'Victoria'
   })
-  return user.save()
-}).then(function(){
+  var trip = new Trip({
+    tripDate: '10/10/2016'
+  })
+  //should be able to associate users and trips
+  user.trips.push(trip);
+  var stop = new Stop({
+    name: 'Restaurant',
+    activities: [{
+      type: 'lifestyle'
+    }]
+  })
+  trip.stops.push(stop);
+  return Promise.all([
+    user.save(),
+    trip.save(),
+    stop.save()
+  ])
+})
+.then(function([user, trip, stop]){
   // find user in db
-  return User.findOne({})
-}).then(function(user){
+  return Promise.all([
+    User.findOne({}),
+    Trip.findOne({}),
+    Stop.findOne({})
+  ])
+})
+.then(function([user, trip, stop]){
   // username should be db if it saved.
-  console.log(user.name, 'Victoria');
+  console.log(user.name, trip.tripDate, stop.name, trip.created, user.created);
+  console.log('user should have a trip', user.trips);
+  console.log('trip should have a stop', trip.stops);
+  console.log('stop should have activities', stop.activities)
 })
 .catch(console.log)
 .then(process.exit)
 
-// clean out test db
-Promise.all([
-  Trip.remove()
-]).then(function(){
-  // we can instatiate and save a user to the db.
-  var user = new User({
-    date: 10/10/2016
-  })
-  return user.save()
-}).then(function(){
-  // find user in db
-  return User.findOne({})
-}).then(function(user){
-  // username should be db if it saved.
-  console.log(trip.date, 10/10/2016);
-})
-.catch(console.log)
-.then(process.exit)
+
