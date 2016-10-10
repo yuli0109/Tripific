dfb7d7b2175733510f480c262e28421e3d03ab7d
Clarissa Bitar <clarissabitar@gmail.com>
2016-10-10 15:21:29 -0700

    finish oauth

--- config/database.js
+++ config/database.js
@@ -5,7 +5,8 @@ mongoose.Promise = Promise;
 var NODE_ENV = process.env.NODE_ENV || 'development'
 
 //todo check database URL has been set on Heroku
-var url = process.env.DATABASE_URL || `mongodb://localhost/tripific_${NODE_ENV}`;
+var url = process.env.DATABASE_URL;
+console.log(url)
 
 mongoose.connect(url);
 
--- config/passport.js
+++ config/passport.js
@@ -6,9 +6,9 @@ var User = require('../models/user');
 
 // verify function
 passport.use(new GoogleStrategy({
-    clientID: `${process.env.GOOGLE_CLIENT_ID}`,
-    clientSecret: `${process.env.GOOGLE_SECRET}`,
-    callbackURL: `${process.env.GOOGLE_CALLBACK}`
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK
   },
   function(accessToken, refreshToken, profile, cb) {
     User.findOne({ 'googleId': profile.id }, function(err, user) {
