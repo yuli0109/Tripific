bad92b8876a6cad703a6cc5aa968529c9af5d1f2
Mondale Felix <mondalefelix@yahoo.com>
2016-10-11 12:37:03 -0700

    fix everything

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,11 +5,6 @@ console.log('linked!');
 var los_angeles = {lat: 34.06, lng: -118.24};
 var waypts = [];
 
-var acOptions = {
-  types: ['establishment']
-};
-var autocomplete = new google.maps.places.Autocomplete(document.getElementById('origin'),acOptions);
-
 
 function initMap() {
   var directionsService = new google.maps.DirectionsService;
@@ -106,25 +101,6 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
       }
     });
 }
-
-google.maps.event.addListener(autocomplete, 'place_changed', function() {
-  infoWindow.close();
-  var place = autocomplete.getPlace();
-  if (place.geometry.viewport) {
-    map.fitBounds(place.geometry.viewport);
-  } else {
-    map.setCenter(place.geometry.location);
-    map.setZoom(17);
-  }
-  marker.setPosition(place.geometry.location);
-  infoWindow.setContent('<div><strong>' + place.name + '</strong><br>');
-  infoWindow.open(map, marker);
-  google.maps.event.addListener(marker,'click',function(e){
-
-    infoWindow.open(map, marker);
-
-  });
-});
 
   // var directionsDisplay = new google.maps.DirectionsRendere({
   //   map: map
