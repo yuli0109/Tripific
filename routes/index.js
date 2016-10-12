f46ef07fea77e9f94be9d42cebe09df9407c6013
Mondale Felix <mondalefelix@yahoo.com>
2016-10-12 09:25:24 -0700

    ADDED SOME ROUTES

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,34 @@ var express = require('express');
 var router = require('express').Router();
 var passport = require('passport');
 
+var Trip = require("../models/trip");
+var User = require("../models/user");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {user: req.user});
 });
 
-router.get('/new_user', function(req, res, next) {
+
+router.get('/new', function(req, res, next) {
   res.render('app');
 });
 
+// Create Stop route
+router.post('/stops'), createstop)
+
+// Delete Stop route
+router.delete('/stops/:id'), removestop)
+
+// Trip Create Route
+router.post('/trips'), create)
+
+// Trip Delete Route
+router.delete('/trips/:id'), del)
+
+// Itnerary route
+router.get('/', function (req,res){
+  res.
+})
 
 module.exports = router;
