25efdb9e8ac442d23dc4a1ae53065d4a235af84f
Mondale Felix <mondalefelix@yahoo.com>
2016-10-12 09:43:18 -0700

    FIXED THE ROUTES AGAIN

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,9 @@ var passport = require('passport');
 var Trip = require("../models/trip");
 var User = require("../models/user");
 
-var tripsCtrl = require('..controllers/trips')
-var stopsCtrl = require('..controllers/stops')
-var usersCtrl = require('..controllers/users')
+var tripsCtrl = require('../controllers/trips')
+var stopsCtrl = require('../controllers/stops')
+var usersCtrl = require('../controllers/users')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,16 +18,16 @@ router.get('/', function(req, res, next) {
 router.get('/trips/:id', tripsCtrl.show)
 
 // Create Stop route
-router.post('/stops' , stopsCtrl.createstop)
+router.post('/stops' , stopsCtrl.createStop)
 
 // Delete Stop route
-router.delete('/stops/:id', stopsCtrl.removestop)
+router.delete('/stops/:id', stopsCtrl.removeStop)
 
 // Trip Create Route
 router.post('/trips' , tripsCtrl.create)
 
 // Trip Delete Route
-router.delete('/trips/:id',  tripsCtrl.del)
+router.delete('/trips/:id',  tripsCtrl.delete)
 
 // Itnerary route
 // // router.get('/', function (req,res){
