a00d8fa0608980df4c7b67122b84f7b1584a2312
Mondale Felix <mondalefelix@yahoo.com>
2016-10-13 15:38:40 -0700

    move static views

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ require('dotenv').config();
 require('./config/database');
 require('./config/passport');
 // view engine setup
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
